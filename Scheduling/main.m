b9cfd7a6d26ef02dd93b4bd353f184574bb1d270
Brian Bian <qbbian@gmail.com>
2014-02-19 22:21:05 -0800

    Initial Commit

---
+++ Scheduling/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Scheduling
+//
+//  Created by Brian Bian on 2/19/14.
+//  Copyright (c) 2014 CIV Graphics, LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
